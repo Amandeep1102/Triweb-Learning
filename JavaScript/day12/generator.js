ab3d1417ea1af3f2eb67b34f818caf8ba737bee9
Amandeep1102 <you@example.com>
2023-08-21 00:41:45 +0530

    Practiced Programs of Day-12 (JS from scratch)

---
+++ JavaScript/day12/generator.js
@@ -0,0 +1,24 @@
+function* makeRangeIterator(start = 0, end = 10, step = 1) 
+{
+    let iterationCount = 0;
+
+    for (let i = start; i < end; i += step) 
+    {
+        iterationCount++;
+        yield i;
+    }
+    return iterationCount;
+}
+
+const myGenerator = makeRangeIterator(0,10);
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
+console.log(myGenerator.next());
