8c3c3eb0b22702a331b7d324a5a084f08ded6e2a
Amandeep1102 <you@example.com>
2023-08-25 19:24:46 +0530

    string , promise and array.

---
+++ ES6/string includes.js
@@ -0,0 +1,5 @@
+// .includes()
+let str = "This is a good place";
+// console.log(str.includes ("Place"));//true
+// console.log(str.startsWith("That"));//false
+console.log(str.endsWith("place")); 
