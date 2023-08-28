145468fca48a67c3cedcf42de94d61db3f580141
Amandeep1102 <you@example.com>
2023-08-29 00:50:33 +0530

    implementation of Try-Catch Asycn-Await and CRUD operations for User

--- NodeJS/workshop2/models/user.js
+++ NodeJS/workshop2/models/user.js
@@ -1,15 +1,55 @@
 
-
 const db = require('../util/database');
 
+module.exports.insert = async (userData) => {
+    // console.log(userData.name);
+    let id = 0;
+    try {
+        let query = "INSERT INTO `users`(`name`, `email`, `password`, `remark`) VALUES (?,?,?,?)";
+        
+        //execute
+        let result = await db.execute(query,[userData.name, userData.email, userData.password, userData.remark]);
+        id = result[0].insertId;
+    } catch (error) {
+        console.log(error);
+    }
+    return id;
+}
 
-module.exports.insert = (userData) => {
-    console.log(userData);
-    console.log(userData.name);
+module.exports.get = async (userData)=>{
 
-    let query = "INSERT INTO `users`(`name`, `email`, `password`, `remark`) VALUES (?,?,?,?);"
-    db.execute(query, [userData.name, userData.email, userData.password, userData.remark]);
+    let retData = '';
+    try {
+        let query = "SELECT * FROM `users` WHERE id=?";
+        let result = await db.execute(query,[userData.id]);
+        retData = result[0][0];
+    } catch (error) {
+        console.log(error);
+    }
+    return retData;
+}
 
 
-    return true;
+module.exports.update = async (userData)=>{
+
+    try {
+        let query = "UPDATE `users` SET `password`=? WHERE id=?";
+        await db.execute(query,[userData.password, userData.id]);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+module.exports.delete = async (userData)=>{
+
+    try {
+        let query = "DELETE FROM `users` WHERE id=?";
+        await db.execute(query,[userData.id]);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
 }
--- NodeJS/workshop2/routers/user.js
+++ NodeJS/workshop2/routers/user.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const userController = require('../controllers/user');
+
 const router = express.Router();
 
-router.post('/register', userController.register);
-router.post('/get', userController.get);
-router.post('/update', userController.update);
-router.post('/delete', userController.delete);
+// /user/register
+router.post('/register',userController.register);
+
+// /user/get
+router.post('/get',userController.get);
+
+// /user/update
+router.post('/update',userController.update);
+
+// /user/delete
+router.post('/delete',userController.delete);
 
 module.exports = router;
