8cf2424382c024eb0f63a1ed68c2dcecda2ecace
Amandeep1102 <you@example.com>
2023-09-11 19:26:25 +0530

    set up quiz controller , Added quiz schema

---
+++ Project/QuizManager/src/controllers/quiz.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express"
+import Quiz from "../models/quiz";
+
+const createQuiz=(req:Request, res:Response) =>{
+    const createQuiz=async (req:Request, res:Response) =>{
+    const quiz=new Quiz(req.body);
+    await quiz.save();
+    res.send(req.body);
+}}
+
+const getQuiz=(req:Request, res:Response) =>{
+    res.send(req.params.quizId);
+}
+
+const updateQuiz=(req:Request, res:Response) =>{
+    res.send(req.body);
+}
+
+const deleteQuiz=(req:Request, res:Response) =>{
+    res.send(req.params.quizId);
+}
+
+const publishQuiz=(req:Request, res:Response) =>{
+    res.send(req.body);
+}
+
+export {createQuiz, getQuiz, updateQuiz, deleteQuiz, publishQuiz};
