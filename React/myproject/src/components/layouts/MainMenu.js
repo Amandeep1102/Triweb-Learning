1ec9b54728190dbf3d063b1354231f80368c837b
Amandeep1102 <you@example.com>
2023-09-19 02:02:53 +0530

    Menu creation in React use Link not anchor tag

--- React/myproject/src/components/layouts/MainMenu.js
+++ React/myproject/src/components/layouts/MainMenu.js
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+
 function MainMenu(){
     return <header>
-        hello
+        <nav>
+            <ul>
+                <li><Link to="/">home</Link></li>
+                <li><Link to="/about">about</Link></li>
+                <li><Link to="/contact">contact</Link></li>
+            </ul>
+        </nav> 
     </header>
 }
 
