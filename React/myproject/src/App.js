adf8dc49d83fe1a6f20307100a2dcaa4584b2ec6
Amandeep1102 <you@example.com>
2023-09-25 02:22:03 +0530

    Edited part where user can store favourite product , counter in navbar for favourite items and also created a seperate page to display favourite items.

--- React/myproject/src/App.js
+++ React/myproject/src/App.js
@@ -4,6 +4,7 @@ import Product from "./pages/product";
 import Layout from "./components/layouts/Layout";
 import Contact from "./pages/Contact";
 import AddProduct from "./pages/AddProduct";
+import FavouriteItems from './pages/FavouriteItems';
 
 import { Routes, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
       <Route path='/contact' element={<Contact/>}></Route>
       <Route path='/product' element={<Product/>}></Route>
       <Route path='/product/add' element={<AddProduct/>}></Route>
+      <Route path='/product/favourite' element={<FavouriteItems />}></Route>
 
       </Routes>
     </Layout>
--- React/myproject/src/components/Product/ProductItem.js
+++ React/myproject/src/components/Product/ProductItem.js
@@ -1,22 +1,46 @@
 import Styles from './ProductItem.module.css';
 import Wrapper from '../layouts/Wrapper';
+import FavouriteContext from '../../store/contextFavourite'
+import { useContext } from 'react'
 
 function ProductItem(props){
-    return <li key={props.item.id}>
-        <Wrapper>
-            <div  className={Styles.image}>
-                <img src={props.item.image} alt="Nothig to display"></img>
-            </div>
-            <div className={Styles.content}>
-                <h2>{props.item.product_name}</h2>
-                <p>{props.item.description}</p>
-                <h4>{props.item.price}</h4>
-            </div>
-            <div className={Styles.btn}>
-                <button>Purchase</button>
-            </div>
-        </Wrapper>
+    
+    const favouriteContext = useContext(FavouriteContext);
+
+    const isFavourite = favouriteContext.isFavouriteItem(props.item._id);
+
+    const toggleFavourite = () => {
+        if (isFavourite) {
+            favouriteContext.removeFavouriteItem(props.item._id);
+        }
+        else {
+            favouriteContext.addFavouriteItem({ ...props.item });
+        }
+    }
+
+    return (
+        <li key={props.item.id}>
+            <Wrapper>
+            <div className={Styles.image}>
+                    <img src={props.item.image} alt="No Image" />
+               </div>
+                <div className={Styles.content}>
+                    <h2>{props.item.product_name}</h2>
+                    <p>{props.item.description}</p>
+                    <h4>{props.item.price}</h4>
+                </div>
+                <div className={Styles.btn}>
+                    <button>Purchase</button>
+                </div>
+                <div>
+                <button className={
+                        isFavourite ? Styles.heartFvrt : Styles.heartUnfvrt                        
+                    }
+                        onClick={toggleFavourite}></button>
+                </div>
+            </Wrapper>
         </li>
+    )
 }
 
 export default ProductItem;
