30b72d0d18e3d24f3756dc87d0550727629cdf20
Amandeep1102 <you@example.com>
2023-09-26 18:49:48 +0530

    created boxes in row form  for tictactoe

---
+++ Project/tic-tac-toe/src/App.js
@@ -0,0 +1,21 @@
+export default function Square() {
+  return (
+    <>
+    <div className="board-row">
+      <button className="square">x</button>
+      <button className="square">x</button>
+      <button className="square">x</button>
+    </div>
+    <div className="board-row">
+      <button className="square">x</button>
+      <button className="square">x</button>
+      <button className="square">x</button>
+    </div>
+    <div className="board-row">
+      <button className="square">x</button>
+      <button className="square">x</button>
+      <button className="square">x</button>
+    </div>
+  </>
+  )
+}
