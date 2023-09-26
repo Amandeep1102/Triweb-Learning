acfb63f52e4d09960ff2b1dc1c70df9e06c3cfcb
Amandeep1102 <you@example.com>
2023-09-26 18:54:51 +0530

    updating square and board function

--- Project/tic-tac-toe/src/App.js
+++ Project/tic-tac-toe/src/App.js
@@ -1,21 +1,24 @@
-export default function Square() {
+ function Square() {
+  return <button className="square">1</button>;
+}
+export default function Board() {
   return (
     <>
-    <div className="board-row">
-      <button className="square">x</button>
-      <button className="square">x</button>
-      <button className="square">x</button>
-    </div>
-    <div className="board-row">
-      <button className="square">x</button>
-      <button className="square">x</button>
-      <button className="square">x</button>
-    </div>
-    <div className="board-row">
-      <button className="square">x</button>
-      <button className="square">x</button>
-      <button className="square">x</button>
-    </div>
-  </>
-  )
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
