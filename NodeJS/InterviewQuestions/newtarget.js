c008730a048ddd9776d196a378d0c8250e403dda
Amandeep1102 <you@example.com>
2023-10-11 19:14:12 +0530

    New Target in Node JS

---
+++ NodeJS/InterviewQuestions/newtarget.js
@@ -0,0 +1,13 @@
+
+function demoNewTarget(x){
+
+    if(new.target = undefined){ console.log("No");
+    }else{
+    this.x = x;
+    console.log("Creating an object")
+    console.log("Inside the function");
+    }
+}
+
+demoNewTarget (5);
+let x = new demoNewTarget (4);
