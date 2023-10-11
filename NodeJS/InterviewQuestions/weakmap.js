6996514db56fd77b637e61e49ddf683c7a41c83f
Amandeep1102 <you@example.com>
2023-10-11 19:14:42 +0530

    weakmap in Node JS

---
+++ NodeJS/InterviewQuestions/weakmap.js
@@ -0,0 +1,16 @@
+﻿
+
+let user = {
+    uname: "Mr adsm",
+    mobile: 435333
+}
+let key1 = {};
+let key2 = {uname: "Amandeep"};
+
+let weakMapExample = {
+
+    [key1]: "Some data",
+    [key2]: "New Data"
+}
+
+console.log(weakMapExample);
