e0bd5eb3d5e2a1f29eaced5b8ce0661cc13b4e0e
Amandeep1102 <you@example.com>
2023-10-11 19:12:59 +0530

    Array Map in Node JS

---
+++ NodeJS/InterviewQuestions/ArrayMap.js
@@ -0,0 +1,14 @@
+﻿
+let arr=[1,2,3,4,5,6,7,8];
+let nearr2 = [];
+
+for(let i=0; i<arr.length; i++){
+    newArr2.push(arr[i]*2);
+}
+
+console.log(newArr2);
+
+console.log("Using MAP()");
+
+let newArrM = arr.map((element) => element*2);
+console.log(newArrM);
